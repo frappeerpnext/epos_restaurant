024769f5da2e9c6a96f9d83617a9d397c73a3a27
frappeerpnext <frapperpnext@gmail.com>
2022-10-21 13:16:51 +0700

    update pos menu

---
+++ epos_restaurant/epos_restaurant/doctype/pos_menu/pos_menu.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, EST Computer and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('POS Menu', {
+	// refresh: function(frm) {
+
+	// }
+});
