02dd3b36754d87e4e0e88628ec04cefcb8545606
frappeerpnext <frapperpnext@gmail.com>
2023-07-13 11:17:37 +0000

    update 2023-07-12 1:41 PM

---
+++ epos_restaurant/epos_restaurant/doctype/test_finger_print/test_finger_print.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, EST Computer and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Test Finger Print', {
+	// refresh: function(frm) {
+
+	// }
+});
